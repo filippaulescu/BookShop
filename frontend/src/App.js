1ac16b5a54040b76af5387fa5da50a892e555697
unknown <filip.paulescu04@gmail.com>
2025-03-10 11:03:44 +0200

    Initializare aplicatie

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">BookShop</a>
+      </header>
+      <main>Lista de carti:</main>
+    </div>
+  );
+}
+
+export default App;
