c46b0e9776ea9460d10f423ffb174f1e1b298107
unknown <filip.paulescu04@gmail.com>
2025-04-01 20:53:32 +0300

    API pentru Sign-in

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
 const data = {
+  users: [
+    {
+      name: 'Filip',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('1234'),
+      isAdmin: true,
+    },
+    {
+      name: 'Marian',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('1234'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       //_id: '1',
